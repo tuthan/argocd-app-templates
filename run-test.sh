c21877d883d4aaee17ed15af5620a83b4b3c2254
Hung Vo <hungvotrung@gmail.com>
2025-01-19 10:27:48 +0700

    Add some argocd support template

---
+++ run-test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+for app_dir in ./*/; do
+  echo
+  echo "[$(date)][$app_dir] Start test"
+  echo
+
+  cd "$app_dir" || exit
+
+  app_name=${PWD##*/}
+
+  for test_file in ./tests/*.yaml; do
+    echo
+    echo "[$(date)][$app_dir] Test case: $test_file"
+    echo
+
+    helm template --debug --values "$test_file" "$app_name" . >"$test_file.output"
+
+    if [ "$1" == "print" ]; then
+      echo
+      cat "$test_file.output"
+      echo
+    fi
+  done
+
+  cd - > /dev/null || exit
+
+  echo
+  echo "[$(date)][$app_dir] Complete test"
+  echo
+done
